e5a1e72bd5b62930f221e50f90af5195f470995a
vinu <vinu@VINUJOSEPH>
2017-01-26 22:22:28 +0530

    Add project files.

---
+++ Angular JS Practice/scripts/Scripts.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="angular.min.js" />
+/// <reference path="angular.js" />
+
+var myApp = angular
+				.module("myModule", [])
+				.controller("myController", function ($scope) {
+				    var employees = [
+						{ name: "Vinu", dateOfBirth: new Date("December 10 1991"), gender: "Male", salary: 55000 },
+						{ name: "Vishnu", dateOfBirth: new Date("January 10 1991"), gender: "Male", salary: 60000 },
+						{ name: "Arjun", dateOfBirth: new Date("March 10 1991"), gender: "Male", salary: 75000 },
+						{ name: "Hiren", dateOfBirth: new Date("July 10 1991"), gender: "Male", salary: 85000 },
+						{ name: "Aaron", dateOfBirth: new Date("October 10 1991"), gender: "Male", salary: 45000 }
+				    ];
+				    $scope.employees = employees;
+				});
